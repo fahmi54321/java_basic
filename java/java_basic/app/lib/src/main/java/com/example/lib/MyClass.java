9736272e918f08fa0c9145e8f96a2854cd79bbe1
fahmiabdulaziz44 <fahmiabdulaziz44@gmail.com>
2025-09-04 16:26:18 +0700

    polymorphism

--- java/java_basic/app/lib/src/main/java/com/example/lib/MyClass.java
+++ java/java_basic/app/lib/src/main/java/com/example/lib/MyClass.java
@@ -3,15 +3,10 @@ package com.example.lib;
 public class MyClass {
     public static void main(String[] args){
 
-        Car myCar = new Car("C300");
+        Vehicle v1 = new Car("Bmw");
+        Vehicle v2 = new Truck(6);
 
-        System.out.println(myCar.getCarModel());
-        myCar.setCarModel("C200");
-
-        System.out.println(myCar.getNumberOfWheels());
-        System.out.println(myCar.getCarModel());
-        myCar.hunk();
-        myCar.start();
-        myCar.stop();
+        v1.start();
+        v2.start();
     }
 }
---
+++ java/java_basic/app/lib/src/main/java/com/example/lib/Truck.java
@@ -0,0 +1,17 @@
+package com.example.lib;
+
+public class Truck extends Vehicle {
+    public Truck(int numberOfWheels) {
+        super(numberOfWheels);
+    }
+
+    @Override
+    void start() {
+        System.out.println("The trucks start");
+    }
+
+    @Override
+    void stop() {
+        System.out.println("The trucks stop");
+    }
+}
